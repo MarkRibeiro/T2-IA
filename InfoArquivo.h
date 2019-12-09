8d55d30a2fd59cd512d30cad1cba449faf2ad909
mark <markpribeiro@hotmail.com>
2019-12-09 09:58:44 -0300

    Comenta codigo

--- InfoArquivo.h
+++ InfoArquivo.h
@@ -1,15 +1,53 @@
 #include "stdio.h"
 
+/************************************
+*       Enum TIPOS
+*   -> Implementada no intuito de numerar os possíveis tipos/classes das flores, e assim facilitar o entendimento do código.
+* 
+*   Valores:
+*    	IRIS_SETOSA = 0,
+*    	IRIS_VERSICOLOUR = 1,
+*    	IRIS_VIRGINICA = 2
+* 
+*************************************/
+
 enum TIPOS {
     IRIS_SETOSA ,
     IRIS_VERSICOLOUR ,
     IRIS_VIRGINICA
 };
+
+/************************************
+*       Enum PROP
+*   -> Implementada no intuito de numerar as possíveis proporçoes dos arquivos, e assim facilitar o entendimento do código.
+* 
+*   Valores:
+*		UM_TERCO = 0,
+*		DOIS_TERCOS = 1,
+*		INTEIRO = 2
+* 
+*************************************/
+
 enum PROP {
 	UM_TERCO ,
 	DOIS_TERCOS ,
 	INTEIRO
 };
+
+/************************************
+*       Estrutura infoArquivo
+*   -> Implementada para armazenar em memória as informações provenientes do arquivo.
+* 
+*   Campos:
+*       1. Sepal Length  =>  sepal length
+*       2. Sepal Width   =>  sepal width
+*       3. Petal Length  =>  petal length
+*       4. Petal Width   =>  petal width
+*		5. Tipo          =>	 class 
+* 
+*   Estrutura escolhida para o armazenamento das distancias foi uma matriz.
+* 
+*************************************/
 
 struct infoArquivo {
 	double sepalLength;
--- main.h
+++ main.h
@@ -3,17 +3,67 @@
 #include <string.h>
 #include "algoritmoKNN.h"
 
+/************************************
+*       Enum Algoritmo
+*   -> Implementada no intuito de numerar os possíveis algoritmos, e assim facilitar o entendimento do código.
+* 
+*   Valores:
+*       KNN = 0,
+*       ADD = 1 
+* 
+*************************************/
+
 enum Algoritmo {
     KNN,
     ADD
 };
 
+/* 
+* contains
+*  Checa se substring está contida na string
+* 
+*  método:
+*       Percorre string buscando por ocorrencias de substring. 
+* 
+*  Parametros:
+*       char *string -> string principal a ser checada 
+*       char *substring -> string a ser buscada na string principal
+*  
+*  Retorno:
+*       Retorna 1 caso a substring esteja presente na string e zero
+*       caso contrário.
+*/
+
 int contains( char *string, char *substring );
+
+/* 
+* atribuindoInfosArquivo
+*  Responsável por armazenar em memória as informações do arquivo de entrada
+* 
+*  método:
+*       Recebe o arquivo inputado pelo usuário, e em seguida aloca uma estrutura
+*       infArq para guardar toda as informações fornecidas pelo arquivo.
+* 
+*  Parametros:
+*       infArq **cmds -> Ponteiro para estrutura de informações do arquivo 
+*       de entrada
+*       FILE *f -> Ponteiro para arquivo de entrada enviado pelo usuário
+*  
+*/
 
 void atribuindoInfosArquivo( infArq *cmds, FILE *f );
 
 void printaArquivo( infArq *cmds, int eleNum );
 
 int embaralhar(infArq *vet, int vetSize);
+
+/* 
+* aviso
+*  Exibe uma mensagem de erro quando chamada
+* 
+*  método:
+*       Usa printf para fazer o display da mensagem no terminal 
+* 
+*/
 
 void aviso();
